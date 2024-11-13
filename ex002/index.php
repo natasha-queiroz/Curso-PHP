0aaa204f66b95ad7acb0aed9978ed5e512113787
Natasha Queiroz <159721737+natasha-queiroz@users.noreply.github.com>
2024-11-12 21:51:46 -0300

    Atualizar o index.php

--- ex002/index.php
+++ ex002/index.php
@@ -6,11 +6,14 @@
     <title>Document</title>
 </head>
 <body>
+
     <h1>exemplo de php</h1>
+
     <?php 
         date_default_timezone_set("America/Fortaleza");
         echo "hoje é dia" . date("d/m/Y");
         echo "e a hora é " . date("G:i:s T");
     ?>
+
 </body>
 </html>
