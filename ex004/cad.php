af08c17e1e23c33d8fc5cc45850f5d900ea215ee
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2024-11-21 19:26:21 -0300

    criado a tela cade.php

---
+++ ex004/cad.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+        <h1>Resultado do processamento</h1>
+</header>
+    <main>
+        <?php
+        $n = $_GET["nome"] ?? "sem nome";
+        $s = $_GET["sobrenome"] ?? "desconhecido";
+
+        echo "<p> é um prazer te conhecer $n $s! este é o meu site";
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a pagina anterior</a></p>
+    </main>
+
+    
+</body>
+</html>
