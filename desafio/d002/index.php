8238fd627480f8d5b1157b25d0403fa98129f266
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2024-11-22 18:42:10 -0300

    d002

---
+++ desafio/d002/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+</head>
+<body>
+    <main>
+        <h1>Trabalhando com numeros aleatorios</h1>
+        <?php 
+            $min = 0;
+            $max = 100;
+
+            echo "<p>Gerando um numero aleatorio entre $min e $max... <br>O valor gerado foi $num </p>"
+        ?>
+        <button onclick="javascript:document.location.reload()">&#xF504; Gerar outro</button>
+    </main>
+</body>
+</html>
