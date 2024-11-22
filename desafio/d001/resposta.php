3e5fc13582e475488854a4d76459b7770c39f770
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2024-11-21 22:50:02 -0300

    d001

---
+++ desafio/d001/resposta.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+</head>
+<body>
+    <main>
+        <h1>Resultado Final</h1>
+        <p>
+            <?php 
+                $n = $_REQUEST["num"] ?? 0;
+                $a = $n-1;
+                $s = $n+1;
+
+                echo "<br> O numero escolhido foi $n";
+                echo "<br> O antecessor é $a";
+                echo "<br> O sucessor é $s";
+            ?>
+        </p>
+        <button onclick="javascipt:window.location.href='index.html'">&#x2B05; Voltar</button>
+    </main>
+</body>
+</html>
