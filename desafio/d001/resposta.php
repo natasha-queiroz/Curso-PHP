38eeb839b5a65f838894d2c0cde7c2214c2cce83
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2024-12-24 15:59:06 -0300

    style

--- desafio/d001/resposta.php
+++ desafio/d001/resposta.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio PHP</title>
     <style>
+        
         main {
             background-color: turquoise;
             height: 500px;
