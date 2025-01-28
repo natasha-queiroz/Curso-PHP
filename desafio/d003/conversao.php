27b471dde80fb59903f4f0c5cd69227bc15ce46f
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2025-01-28 19:38:44 -0300

    ajustes nos codigos

--- desafio/d003/conversao.php
+++ desafio/d003/conversao.php
@@ -1,12 +1,29 @@
-<?php 
-    $cotação = 5.90;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+</head>
+<body>
+    <main>
+        <?php 
+            //cotação do Google
+            $cotação = 5.90;
 
-    $real = $_REQUEST["din"];
+            //Quanto $$ vc tem?
+            //?? 0; -> se nao digitar nada vai ser 0
+            $real = $_REQUEST["din"] ?? 0;
 
-    $dólar = $real / $cotação;
+            //equivalencia em dolar
+            $dólar = $real / $cotação;
 
-    $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+            //formatação de moedas com internacionalização
+            $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-    echo "Seus " . numfmt_format_currency($padrão, $real, "BRL") . "equivale a " . numfmt_format_currency($padrão, $dólar, "USD");
+            echo "Seus " . numfmt_format_currency($padrão, $real, "BRL") . "equivale a " . numfmt_format_currency($padrão, $dólar, "USD");
 
-?>
+        ?>
+    </main>
+</body>
+</html>
--- desafio/d003/index.php
+++ desafio/d003/index.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio PHP</title>
+    <style>
+        
+    </style>
 </head>
 <body>
     <main>
         <h1>conversor de moedas</h1>
         <form action="conversao.php" method="get">
             <label for="din">Quantos R$ vc tem na carteira?</label>
-            <input type="number" name="din" id="din" step="0.01">
+            <input type="number" name="din" id="din" step="0.01"> <!-- step = para valores com decimais  -->
             <input type="submit" value="Converter">
         </form>
     </main>
