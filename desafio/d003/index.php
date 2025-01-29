18405fdf10ec4b93a07052be0e94bdaed9f3315f
Natasha Queiroz <natashaqueiroz1612@gmail.com>
2025-01-29 20:48:09 -0300

    d003

--- desafio/d003/index.php
+++ desafio/d003/index.php
@@ -10,12 +10,15 @@
 </head>
 <body>
     <main>
+
         <h1>conversor de moedas</h1>
+
         <form action="conversao.php" method="get">
             <label for="din">Quantos R$ vc tem na carteira?</label>
             <input type="number" name="din" id="din" step="0.01"> <!-- step = para valores com decimais  -->
             <input type="submit" value="Converter">
         </form>
+        
     </main>
 </body>
 </html>
